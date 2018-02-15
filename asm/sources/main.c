d33466a973961dcecbc50d640289f05f2aafaf84
Antoine HERBOSA <aherbosa@localhost.localdomain>
2018-02-15 17:12:22 +0100

    modify makefile

--- asm/sources/main.c
+++ asm/sources/main.c
@@ -5,3 +5,16 @@
 ** corewar
 */
 
+#include "asm.h"
+
+int main(int argc, char **argv)
+{
+	int fd = 0;
+
+	if (argc != 2)
+		return (84);
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+		return (84);
+	return (0);
+}
