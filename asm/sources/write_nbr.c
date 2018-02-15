f83de15a58ff19d64d461f4d7f3c07d13f56f498
Antoine HERBOSA <aherbosa@localhost.localdomain>
2018-02-15 15:47:31 +0100

    add include in previous function

--- asm/sources/write_nbr.c
+++ asm/sources/write_nbr.c
@@ -5,30 +5,10 @@
 ** main.c
 */
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
+#include "asm.h"
                        
-
-# define PROG_NAME_LENGTH        128
-# define COMMENT_LENGTH          2048
-
-struct header_s
-{
-   int  magic;
-# define COREWAR_EXEC_MAGIC      0xea83f3        /* why not */
-   char prog_name[PROG_NAME_LENGTH + 1];
-   int  prog_size;
-   char comment[COMMENT_LENGTH + 1];
-};
-
 int write_nbr(int nbr, int fd)
 {
-	int i = 0;
-	int j = 0;
 	int wrt = 0;
 	int mem = nbr;
 
@@ -48,22 +28,5 @@ int write_nbr(int nbr, int fd)
 	wrt = write(fd, &nbr, 1);
 	if (wrt == -1)
                 return (-1);
-	return (0);
-}
-
-
-
-int main(int argc, char **argv)
-{
-	struct header_s stc;
-	int i = 0;
-        int fd = 0;
-        int wrt = 0;
-
-        fd = open("data_test", (O_CREAT)|O_RDWR, S_IRWXU);
-        if (fd == 0)
-                return (84);
-	int a = 255;
-	write_nbr(a, fd);
 	return (0);
 }
