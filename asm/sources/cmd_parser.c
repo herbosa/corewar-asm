6b92740074d845f7dd3eee9f0e72a512abdd886a
Antoine HERBOSA <aherbosa@localhost.localdomain>
2018-03-01 09:07:52 +0100

    correct add

--- asm/sources/cmd_parser.c
+++ asm/sources/cmd_parser.c
@@ -628,16 +628,18 @@ int cmd_add_second_param(char ***inst, wrt_t ***wrt_nbr, int i,
 			int param)
 {
 	if (my_strncmp(inst[i][2], "r", 1) == 1) {
+		inst[i][2]++;
 		wrt_nbr[i][2]->nbr = my_atoi(inst[i][2]);
-		wrt_nbr[i][2]->size = 2;
+		wrt_nbr[i][2]->size = 1;
 		param = param + 100;
 	} else {
 		my_puterr("invalid instruction in add");
 		exit(84);
 	}
 	if (my_strncmp(inst[i][3], "r", 1) == 1) {
+		inst[i][3]++;
 		wrt_nbr[i][3]->nbr = my_atoi(inst[i][3]);
-		wrt_nbr[i][3]->size = 2;
+		wrt_nbr[i][3]->size = 1;
 		param = param + 1;
 	} else {
 		my_puterr("invalid instruction in add");
@@ -653,8 +655,9 @@ void cmd_add(char ***inst, wrt_t ***wrt_nbr, int i)
 	wrt_nbr[i][0]->nbr = 4;
 	wrt_nbr[i][0]->size = 1;
 	if (my_strncmp(inst[i][1], "r", 1) == 1) {
+		inst[i][1]++;
 		wrt_nbr[i][1]->nbr = my_atoi(inst[i][1]);
-		wrt_nbr[i][1]->size = 2;
+		wrt_nbr[i][1]->size = 1;
 		param = param + 10000;
 	} else {
 		my_puterr("invalid instruction in add");
